be2bcf8bebaddf2b418f30ec0fd1339e6f2115ae
ashfinite <140586561+ashfinite@users.noreply.github.com>
2025-03-27 15:47:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_lsL66RwZI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'uosq1etqnsnpavquriti3hb0o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_iJ4uuSxgX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17c8b9r9m2tbh1nplku54fahto', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
